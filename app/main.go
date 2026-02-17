79883b63eb00c6ae36ca4114057e850d65cdb217
= <=>
2026-02-17 20:57:07 +0530

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -5,6 +5,7 @@ import (
 	"errors"
 	"fmt"
 	"os"
+	"os/exec"
 	"path/filepath"
 	"strings"
 )
@@ -56,7 +57,13 @@ func main() {
 					fullPath := filepath.Join(path, param)
 					res := fileExistsAndPermission(fullPath)
 					if res == StatusExecutable {
-						fmt.Println(param, "is", fullPath)
+						// fmt.Println(param, "is", fullPath)
+
+						cmd := exec.Command(fullPath, args...)
+
+						if err := cmd.Run(); err != nil {
+							fmt.Println("Error", err)
+						}
 						break LoopLabel
 					} else if res == StatusNotFound {
 						continue
